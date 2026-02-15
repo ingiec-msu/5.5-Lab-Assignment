dd428fbda676614b345548060d11f71b24e76517
ingiec-msu <charl112@msu.edu>
2026-02-15 16:25:26 +0000

    Switched the original react and vite logos to pickle rick pictures.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PickleRick from './assets/PickleRick.webp'
+import RickTape from './assets/RickTape.png'
 import './App.css'
 
 function App() {
@@ -10,13 +10,13 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={RickTape} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={PickleRick} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Pickle Rick!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
