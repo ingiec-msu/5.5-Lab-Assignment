159934fab1db96f472231a87efc9318504529db4
ingiec-msu <charl112@msu.edu>
2026-02-22 02:26:31 +0000

    Compoenent 1 in progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,30 @@ import PickleRick from './assets/PickleRick.webp'
 import RickTape from './assets/RickTape.png'
 import './App.css'
 
+/* Data structure */
 const items = [
   { id: 1, name: 'Rick and Morty 20" Pickle Rick Plush Pillow', price: '$47.99', isInStock: true },
   { id: 2, name: 'Rick and Morty: Talking Pickle Rick', price: '$13.79', isInStock: true },
   { id: 3, name: 'Rick And Morty Pickle Rick™ Dill Flavored Pickle', price: '$2.00', isInStock: false },
 ];
+
+/* Compoenent 1; Display Items*/
+function Merch() {
+  const listMerch = items.map(item =>
+    <li
+      key={item.id}
+      style={{
+        color: item.isAvailable ? 'red' : 'green'
+      }}
+    >
+      {item.name}
+    </li>
+  );
+  return (
+    <ul>{listMerch}</ul>
+  )
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
