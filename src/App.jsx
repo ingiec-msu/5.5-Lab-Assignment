46e3e8e7fdb3f2c6f1ad66ec14e1c437f5abb11f
ingiec-msu <charl112@msu.edu>
2026-02-22 13:46:57 +0000

    Added image, price, and name of compoenent 1.

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,32 @@ import './App.css'
 
 /* Data structure */
 const items = [
-  { id: 1, name: 'Rick and Morty 20" Pickle Rick Plush Pillow', price: '$47.99', isInStock: true },
-  { id: 2, name: 'Rick and Morty: Talking Pickle Rick', price: '$13.79', isInStock: true },
-  { id: 3, name: 'Rick And Morty Pickle Rick™ Dill Flavored Pickle', price: '$2.00', isInStock: false },
+  { id: 1, name: 'Rick and Morty 20" Pickle Rick Plush Pillow', price: '$47.99', isInStock: true, img: 'src/assets/images/Plush Pillow.avif'},
+  { id: 2, name: 'Rick and Morty: Talking Pickle Rick', price: '$13.79', isInStock: true, img: 'src/assets/images/Talking Pillow.webp'},
+  { id: 3, name: 'Rick And Morty Pickle Rick™ Dill Flavored Pickle', price: '$2.00', isInStock: false, img: 'src/assets/images/Dill Pickle.jpg'},
 ];
 
 /* Compoenent 1; Display Items*/
 function Merch() {
   const listMerch = items.map(item =>
-    <li
-      key={item.id}
-      style={{
-        color: item.isInStock ? 'green' : 'red'
-      }}
-    >
-      {item.name}
-    </li>
+    <div>
+      {/* Components can't return multiple JSX tags unless they are wrapped in a parent element */}
+      {/* This is a JSX comment */}
+      <h2>{items.name}</h2>
+      <p>{items.price}</p>
+      {/* This a conditional that checks if an image exists before displaying it */}
+      {items.image &&
+      <img
+        className="bookCover"
+        src={book.image}
+        //alt={book.title + ' cover'}
+        style={{
+          width: items.width,
+          height: items.height
+        }}
+      />
+      }
+    </div>
   );
   return (
     <ul>{listMerch}</ul>
